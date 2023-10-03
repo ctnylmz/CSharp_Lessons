389169d762680ef67acf2a5a030abbfbf34804e2
ctnylmz <143901863+ctnylmz@users.noreply.github.com>
2023-10-03 15:03:45 +0300

    2 Day CSharp Camp kodlama.io/

---
+++ CSharp_Camp/array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] course = new string[]
+            {
+                "CSharp Camp",
+                "Java Camp",
+                "NodeJS Camp",
+                "React Camp",
+                "Pythone Camp"
+            };
+
+
+            /*
+            for (int i = 0; i <= course.Length; i++)
+            {
+                Console.WriteLine(course[i]);
+            }
+            */
+
+            foreach (var item in course)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CSharp_Camp/ifElse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ifElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "İhtiyaç Kredesi";
+
+            int krediTutari = 10000;
+
+            double faizOranı = 0.45;
+
+            double toplamFaiz = krediTutari * faizOranı;
+
+            bool giris = false;
+
+            if (giris == true)
+            {
+                Console.WriteLine(kategoriEtiketi + " Kredi Onaylandı!");
+                Console.WriteLine(krediTutari + "TL Geri Ödeme ile " + (krediTutari + toplamFaiz) + "TL");
+            }
+            else if (giris == false)
+            {
+                Console.WriteLine("Lütfen Giriş Yapın");
+            }
+            else
+            {
+                Console.WriteLine("Hata Lütfen İletişime Geçin");
+            }
+        }
+    }
+
+}
