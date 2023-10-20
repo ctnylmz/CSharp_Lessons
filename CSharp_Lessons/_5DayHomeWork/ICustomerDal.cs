392d671db096ea779b5d6c1dadf01eedb143e05c
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-10-20 08:56:19 +0300

    (5 Days Homework) (CSharp OOP)

---
+++ CSharp_Lessons/_5DayHomeWork/ICustomerDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5DayHomeWork
+{
+    interface ICustomerDal
+    {
+        void Creat();
+        void Read();
+        void Update();
+        void Delete();
+
+    }
+
+
+
+}
---
+++ CSharp_Lessons/_5DayHomeWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _5DayHomeWork
+{
+    internal class Program
+    {
+        //Class
+        static void Main(string[] args)
+        {
+        
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Çetin";
+            customer.LastName = "Yılmaz";
+            customer.City = "İstanbul";
+
+            Console.WriteLine(customer.FirstName + " " + customer.LastName );
+
+
+
+        }
+    }
+  
+}
